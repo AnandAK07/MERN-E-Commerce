2a1e525f02dfdb984612c59a66b1c25cbf1f96c7
AnandAK07 <anandkumark055@gmail.com>
2024-02-01 22:19:23 +0530

    product routes done

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,27 @@
 const express = require('express');
 const connection=require('./configs/db')
 const userRouter=require('./controllers/user.route')
+const productRouter=require('./controllers/product.route');
+const cartRouter = require('./controllers/cart.route');
+const cors=require('cors')
+
 const dotenv = require('dotenv');
 dotenv.config();
 
 
 const app=express();
+app.use(cors())
 app.use(express.json())
+
 
 app.get('/',(req,res)=>{
     res.send("App is working")
 })
 
 app.use('/users', userRouter)
+app.use('/product', productRouter)
+app.use('/cart',cartRouter)
+
 
 const PORT=process.env.PORT
 app.listen(PORT,async()=>{
