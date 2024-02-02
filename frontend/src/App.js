5105ea5befa62c1c0b4fda015c7fd2d5d1016374
AnandAK07 <anandkumark055@gmail.com>
2024-02-02 20:50:03 +0530

    Cart added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
-
+import { Navbar } from './components/navbar/Navbar';
 import { ProductPage } from './pages/ProductPage';
 
 function App() {
   return (
     <div className="">
- 
+      <Navbar />
       <ProductPage />
     </div>
   );
