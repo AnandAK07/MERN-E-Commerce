56cac79744868fc1d68d8810c8a28cd95ec85e6e
AnandAK07 <anandkumark055@gmail.com>
2024-02-26 22:45:15 +0530

    optimizing code

--- backend/controllers/paymentController.js
+++ backend/controllers/paymentController.js
@@ -9,36 +9,30 @@ const instance = new Razorpay({
 
 const checkout=async(req,res)=>{
     const { productIds, amount } = req.body;
-    console.log(productIds, amount, 'n')
-    // console.log(name, amount)
 
     const order = await instance.orders.create({
         amount: Number(amount * 100),  // amount in the smallest currency unit
         currency: "INR",
         receipt: "order_rcptid_11"
     })
-    // console.log(order)
 
     await orderModel.create({
         order_id: order.id,
         productIds: productIds,
         amount: amount
     })
-    console.log({ order })
     res.json({ order })
 }
 
 const verification=async(req,res)=>{
     const { razorpay_payment_id, razorpay_order_id, razorpay_signature } = req.body;
-    console.log(razorpay_payment_id, razorpay_order_id, razorpay_signature, 'cver')
-
+    
     const body_data = razorpay_order_id + "|" + razorpay_payment_id
 
     const expect = crypto.createHmac('sha256', '0recQZFK4GnoZ42ajE0HDBBS').update(body_data).digest('hex')
 
     const isValid = expect === razorpay_signature
     if (isValid) {
-        console.log('v,,,,')
         await orderModel.findOneAndUpdate(
             { order_id: razorpay_order_id },
             {
--- backend/routes/payment.route.js
+++ backend/routes/payment.route.js
@@ -1,14 +1,9 @@
 const express = require('express');
 const payment = express.Router();
 const { checkout, verification }=require('../controllers/paymentController')
-// const { signup, login, addAllProduct } = require('../controllers/userController');
 
 
 payment.post('/checkout', checkout)
 payment.post('/verification', verification)
-
-// payment.post('/add', addAllProduct)
-// payment.post('/signup', signup);
-// payment.post('/login', login);
 
 module.exports = payment;
