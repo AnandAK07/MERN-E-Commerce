87b10547181742bb0241e65ade025749f265fa50
Your Name <you@example.com>
2025-08-13 17:26:32 +0530

    fix(carousel): replaced external image URLs with local asset imports for consistent display and proper sizing

--- frontend/src/components/home/MainCarousel.jsx
+++ frontend/src/components/home/MainCarousel.jsx
@@ -1,25 +1,28 @@
 import React from 'react'
 import AliceCarousel from 'react-alice-carousel';
 import 'react-alice-carousel/lib/alice-carousel.css';
+import image1 from "../../assets/images/carousel1.jpg"
+import image2 from "../../assets/images/carousel2.jpg"
+
 
 export const mainCaroselData = [
+    // {
+    //     name: "dress",
+    //     image: "https://www.ethnicplus.in/cdn/shop/files/1_6c18ba97-1808-4b5f-b40d-0a73e98c6e6b.jpg?v=1755022934",
+    // },
     {
         name: "dress",
-        image: "https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/1/0/10_5.jpg",
-    },
-    {
-        name: "dress",
-        image: "https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/1/2/12_4.jpg",
+        image: image1,
 
     },
     {
         name: "dress",
-        image: "https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/9/_/9_8.jpg",
+        image: image2,
     },
-    {
-        name: "dress",
-        image: "https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/1/1/11_4.jpg",
-    }
+    // {
+    //     name: "dress",
+    //     image: "https://www.ethnicplus.in/cdn/shop/files/25_gown_coll_copy.jpg_mob_2_1920x.jpg?v=1754073611",
+    // }
 ]
 const responsive = {
     0: { items: 1 },
